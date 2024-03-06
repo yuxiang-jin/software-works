961ce22cd225bf09341ca09495cb22cf51ed226a
靳玉祥 <67530219+yuxiang-jin@users.noreply.github.com>
2024-03-06 22:46:18 +0800

    Delete librarymanage-v2 directory

--- librarymanage-v2/src/main/resources/schema-mysql.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists book.book_inf;
--- 创建book_inf表
-create table book.book_inf
-(
-    book_id int primary key auto_increment,
-    book_title varchar(255) not null,
-    book_author varchar(255),
-    book_price double
-);
